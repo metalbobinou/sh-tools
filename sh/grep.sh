468b4464ad2daca742aa2879be2234fecce68f33
fabrice.boissier <fabrice.boissier@epita.fr>
2023-07-21 12:06:55 +0200

    Printer around lines

--- sh/grep.sh
+++ sh/grep.sh
@@ -11,3 +11,9 @@ grep -o "Monday" file.txt
 
 # Count lines containing lines that only match a pattern
 grep -o "Monday" file.txt | wc -l
+
+# Print 3 lines before and 2 lines after a match [BSD & GNU grep]
+grep -B 3 -A 2 "MATCH" file.txt
+
+# Print 5 lines before and after a match
+grep -C 5 "MATCH" file.txt
